40b2e322618e697fac044120714dd5be20569fcb
luizike2002 <luiz.grazziotin@gmail.com>
2023-08-30 20:33:40 -0300

    primeiro commit

---
+++ exercicios/exe01/Program.cs
@@ -0,0 +1,12 @@
+﻿int[] vetor = {-5, 13, -90, 99, 326, -34, 67, -81, 777, 0};
+Array.Sort(vetor);
+foreach (int i in vetor)
+{
+    Console.WriteLine("Ordenando...");
+    Console.WriteLine(i);
+}
+Array.Sort(vetor);
+foreach (int i in vetor)
+{
+    Console.WriteLine(i);
+}
---
+++ segundaaula/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Globalization;
+string[] months = new string[12];
+
+for(int month = 1; month <- 12; month++)
+{
+    DateTime FirstDay = new DateTime(DateTime.Now.Year, month, 1);
+    string name = FirstDay.ToString("MMMM",
+    CultureInfo.CreateSpecificCulture("en"));
+}
+
+foreach (string month in mouths)
+{
+    Console.WriteLine($"-> {month}");
+}
